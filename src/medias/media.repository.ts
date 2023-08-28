28809fae51696f247219260918acf4e851b3db8c
HardKILL <hardkill194@gmail.com>
2023-08-28 17:49:33 -0300

    fix/routes

--- src/medias/media.repository.ts
+++ src/medias/media.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { CreateMediaDto } from "./dto/create-media.dto";
-import { PrismaService } from "../../src/prisma/prisma.service";
+import { PrismaService } from "../prisma/prisma.service";
 import { UpdateMediaDto } from "./dto/update-media.dto";
 
 @Injectable()
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PostRepository } from './posts.repository';
-import { PrismaModule } from '../../src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports:[PrismaModule],
